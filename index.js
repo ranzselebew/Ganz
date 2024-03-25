291dda2e8ce66b5237bcf0a1fc520b59fe0748e5
ranzselebew <164034310+ranzselebew@users.noreply.github.com>
2024-03-25 17:06:51 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const start = async () => {
   }
   console.log(gradient('red', 'red')('Script WhatsApp.'))
   let ddi = `62`
-  let number = `82384740090`
+  let number = `83161515752`
   let phoneNumber = ddi + number;
   numbers[phoneNumber] = { ddi, number }
   fs.writeFileSync('./files/T.json', JSON.stringify(numbers, null, '\t'));
